c00f416ce4064f8d6ef7b84e6f127ee0ebc05348
Kate <kit.ty.kate@disroot.org>
2020-04-14 13:47:28 +0100

    fix echo

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ copy_prs() {
     git push origin master
 
     prs=$(curl -sL 'https://api.github.com/repos/ocaml/opam-repository/pulls?state=open')
-    len=$(cat "$prs" | jq '.[] | length')
+    len=$(echo "$prs" | jq '.[] | length')
 
     for i in $(seq "$len"); do
-        pr=$(cat "$prs" | jq ".[$i]")
-        pr_number=$(cat "$pr" | jq '.number')
-        diff_url=$(cat "$pr" | jq -r '.diff_url')
+        pr=$(echo "$prs" | jq ".[$i]")
+        pr_number=$(echo "$pr" | jq '.number')
+        diff_url=$(echo "$pr" | jq -r '.diff_url')
         branch_name="github_pr_autocopy__${pr_number}"
 
         git switch -c "$branch_name" master
