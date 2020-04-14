cd5bfc68a6057a29d13a5e7d573a6e1682e45fa2
Kate <kit.ty.kate@disroot.org>
2020-04-14 15:40:20 +0100

    Give links to PRs to open in the destrination repository

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ git push origin master
 
 prs=$(curl -sL "https://api.github.com/repos/$src/pulls?state=open")
 len=$(echo "$prs" | jq 'length')
+prs_to_open=
 
 for i in $(seq "$len"); do
     i=$(echo "$i - 1" | bc)
@@ -30,8 +31,11 @@ for i in $(seq "$len"); do
         git switch "$branch_name"
     else
         git switch -c "$branch_name" master
+        prs_to_open="${prs_to_open:+$prs_to_open }${branch_name}"
     fi
+
     patch=$(curl -sL "$diff_url")
+
     if echo "$patch" | patch -p1 -N -s; then
         git add *
         git commit -m 'test'
@@ -46,4 +50,12 @@ for i in $(seq "$len"); do
         git commit -m 'test'
         git push --force-with-lease origin "$branch_name"
     fi
+done
+
+echo
+echo
+echo 'The following PRs are to be opened:'
+
+for branch in $prs_to_open; do
+    echo "* https://github.com/${dst}/compare/master...${branch}?expand=1"
 done
